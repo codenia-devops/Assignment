66df8d6f9f5153be00e0debe8eec832623afe9b2
Codenia Technologies LLP <93813823+codenia-devops@users.noreply.github.com>
2023-02-17 16:17:03 +0530

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,101 @@
+<?php
+// including the database connection file
+include_once("config.php");
+
+if(isset($_POST['update']))
+{	
+
+	$id = mysqli_real_escape_string($mysqli, $_POST['id']);
+	
+	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
+	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
+	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+	$title = mysqli_real_escape_string($mysqli, $_POST['title']);
+	$address = mysqli_real_escape_string($mysqli, $_POST['address']);
+	$salary = mysqli_real_escape_string($mysqli, $_POST['salary']);	
+	
+	// checking empty fields
+	if(empty($name) || empty($age) || empty($email)) {	
+			
+		if(empty($name)) {
+			echo "<font color='red'>Name field is empty.</font><br/>";
+		}
+		
+		if(empty($age)) {
+			echo "<font color='red'>Age field is empty.</font><br/>";
+		}
+		
+		if(empty($email)) {
+			echo "<font color='red'>Email field is empty.</font><br/>";
+		}		
+	} else {	
+		//updating the table
+		$result = mysqli_query($mysqli, "UPDATE users SET name='$name',age='$age',email='$email',title='$title',address='$address',salary='$salary' WHERE id=$id");
+		
+		//redirectig to the display page. In our case, it is index.php
+		header("Location: index.php");
+	}
+}
+?>
+<?php
+//getting id from url
+$id = $_GET['id'];
+
+//selecting data associated with this particular id
+$result = mysqli_query($mysqli, "SELECT * FROM users WHERE id=$id");
+
+while($res = mysqli_fetch_array($result))
+{
+	$name = $res['name'];
+	$age = $res['age'];
+	$email = $res['email'];
+
+	$title = $res['title'];
+	$address = $res['address'];
+	$salary = $res['salary'];
+	
+}
+?>
+<html>
+<head>	
+	<title>Edit Data</title>
+</head>
+
+<body>
+	<a href="index.php">Home</a>
+	<br/><br/>
+	
+	<form name="form1" method="post" action="edit.php">
+		<table border="0">
+			<tr> 
+				<td>Name</td>
+				<td><input type="text" name="name" value="<?php echo $name;?>"></td>
+			</tr>
+			<tr> 
+				<td>Age</td>
+				<td><input type="text" name="age" value="<?php echo $age;?>"></td>
+			</tr>
+			<tr> 
+				<td>Title</td>
+				<td><input type="text" name="title" value="<?php echo $title;?>"></td>
+			</tr>
+			<tr> 
+				<td>Email</td>
+				<td><input type="text" name="email" value="<?php echo $email;?>"></td>
+			</tr>
+			<tr> 
+				<td>Address</td>
+				<td><input type="text" name="address"  value="<?php echo $address;?>"></td>
+			</tr>
+			<tr> 
+				<td>Salary</td>
+				<td><input type="text" name="salary"  value="<?php echo $salary;?>"></td>
+			</tr>
+			<tr>
+				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+				<td><input type="submit" name="update" value="Update"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ test.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 17, 2023 at 11:37 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `salary` varchar(200) DEFAULT NULL,
+  `age` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `title`, `email`, `address`, `salary`, `age`) VALUES
+(2, 'Ravi', 'PHP developer', 'ravi@gmail.com', 'india', '900000', 28);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
